9960371e0b50c5e8211b10cfc6a01198f1aec346
AnNurrr <ainuremilbekova9@gmail.com>
2023-12-28 16:22:01 -0600

    Java exercise

---
+++ moduletest/Module1/src/forEach.java
@@ -0,0 +1,11 @@
+public class forEach {
+    public static void main(String[] args) {
+        String [] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        for(String week: daysOfWeek) {
+            System.out.println(week);
+
+        }
+        System.out.println("On winter quarter, I study on Tuesday and Wednesday");
+    }
+}
