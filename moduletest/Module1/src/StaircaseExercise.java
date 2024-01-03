a5726f90c72a3236d68ce2050ef1526bd3b8a7df
AnNurrr <ainuremilbekova9@gmail.com>
2024-01-03 14:35:44 -0600

    exercise

---
+++ moduletest/Module1/src/StaircaseExercise.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StaircaseExercise {
+    public static void main(String[] args) {
+
+        // printing numbers as tree
+        for(int i=1; i<=9; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
